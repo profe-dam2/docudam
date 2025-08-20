9e3812ad1816f6a88d4645cee2bedd383ee268fc
profe.dam2 <profe.dam2@gmail.com>
2025-08-20 11:34:53 +0200

    cambio

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Desarrollo de interfaces',
+  title: 'Desarrollo de Interfaces',
   tagline: 'Desarrollo de aplicaciones multiplataforma',
   favicon: 'img/favicon.ico',
 
