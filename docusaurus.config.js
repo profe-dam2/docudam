9239abfcd353c0cccd1975b943f261dbfb1c21ce
profe.dam2 <profe.dam2@gmail.com>
2025-08-20 12:30:09 +0200

    Cambios

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -91,7 +91,7 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Avalonia',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
